9599e9288416cdd6723cb4e3e4e9c3e9f4f47b00
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-08-18 01:47:39 +0300

    commit tree 3903544bf0b00961f6f8b906d8716c88867e18dd

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (names_array)
+    names_array.map do |name|
+        badge_maker (name)
+    end 
+end
+
+def assign_rooms (names_array)
+    room = [1,2,3,4,5,6,7]
+    names_array.map.with_index do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{room[index]}!"
+     end
+end
+
+# assign_rooms (["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+
+def printer (names_array)
+    batch_badge_creator(names_array).map do |message|
+       puts message
+    end
+    assign_rooms(names_array).map { |message| puts message}
+end
+
+printer(["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
